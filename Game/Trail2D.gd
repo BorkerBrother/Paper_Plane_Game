2a5ecbb7b266bf5e0aa8fb406bbeb5f969609637
fghwer <j_olth01@uni-muenster.de>
2023-11-04 13:12:47 +0100

    constant v in y + trail position fix + player scaled down + background scaled in y +  new bg test

--- Game/Trail2D.gd
+++ Game/Trail2D.gd
@@ -1,14 +1,17 @@
 extends Line2D
 
-@export var length = 30
+@export var length = 10
 var point = Vector2()
+var offset = Vector2()
 
 func _process(delta):
 	global_position = Vector2(0,0)
 	global_rotation = 0
 	
 	point = get_parent().global_position
+	offset = get_parent().transform.x.rotated(-PI*1.0/4.0)
 	
+	point -= offset*10
 	add_point(point)
 	while get_point_count()>length:
 		remove_point(0)
